3563bfda1cc7bae3e71971edc6a0a9ffa7de8e85
Muhammad Khurram <khurram33250@gmail.com>
2017-12-04 20:05:13 +0500

    done some changings

---
+++ app/src/main/java/com/example/hasham/movies_mvvm/util/RecyclerBindingAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.hasham.movies_mvvm.util
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import java.util.AbstractList
+import java.util.ArrayList
+
+/**
+ * Developed by Hasham.Tahir on 10/17/2016.
+ */
+
+class RecyclerBindingAdapter<T>(private val holderLayout: Int, private val variableId: Int, items: AbstractList<T>) : RecyclerView.Adapter<RecyclerBindingAdapter.BindingHolder>() {
+    private var items: AbstractList<T> = ArrayList()
+    private var onItemClickListener: OnItemClickListener<T>? = null
+
+    init {
+        this.items = items
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerBindingAdapter.BindingHolder {
+        val v = LayoutInflater.from(parent.context)
+                .inflate(holderLayout, parent, false)
+        return BindingHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerBindingAdapter.BindingHolder, position: Int) {
+        val item = items[position]
+        holder.binding.root.setOnClickListener {
+            if (onItemClickListener != null)
+                onItemClickListener!!.onItemClick(position, item)
+        }
+
+        holder.binding.setVariable(variableId, item)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener<T>) {
+        this.onItemClickListener = onItemClickListener
+    }
+
+    fun updateList(list: AbstractList<T>) {
+        this.items = list
+        notifyDataSetChanged()
+    }
+
+    interface OnItemClickListener<T> {
+        fun onItemClick(position: Int, item: T)
+    }
+
+    internal class BindingHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val binding: ViewDataBinding
+
+        init {
+            binding = DataBindingUtil.bind(v)
+        }
+    }
+}
